cf7df4a2b62d8da224f8ac1d8cc362d9aeb4c262
Rafael Linhares <rafaellinharescunha@gmail.com>
2025-02-15 15:17:42 -0300

    Corrigido entry-point para Cloudflare Workers

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
- export default {
+export default {
   async fetch(request) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'cors-anywhere.herokuapp.com'; // Pode alterar para outro proxy
-      return fetch(url, request);
-    }
-    return new Response('CORS Proxy Running', { status: 200 });
-  }
+    return new Response("CORS Proxy Running", {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+      },
+    });
+  },
 };
-
